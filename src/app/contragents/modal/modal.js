77aed856795a9eedda38209f845a46da53574447
rshinov <rshinov@moysklad.ru>
2024-07-18 17:52:27 +0500

    замечания pr

--- src/app/contragents/modal/modal.js
+++ src/app/contragents/modal/modal.js
@@ -10,55 +10,6 @@ export default function (container, data, callback) {
         return component;
     }
 
-    function handleSubmit(event, form, modal) {
-        event.preventDefault();
-
-        const formData = new FormData(form);
-
-        if (!validateField(formData.get("inn"), /^\d{11}$/, 'innError')) {
-            return;
-        }
-
-        if (!validateField(formData.get("kpp"), /^\d{9}$/, 'kppError')) {
-            return;
-        }
-
-        const record = createRecord(formData);
-        data.push(record);
-
-        callback();
-        form.reset();
-        closeModal(modal);
-    }
-
-    function validateField(value, regex, errorElementId) {
-        const errorElement = document.getElementById(errorElementId);
-        if (!regex.test(value)) {
-            errorElement.classList.remove("hidden");
-            return false;
-        } else {
-            errorElement.classList.add("hidden");
-            return true;
-        }
-    }
-
-    function createRecord(formData) {
-        const record = {};
-        formData.forEach((value, key) => {
-            record[key] = value;
-        });
-        record.id = crypto.randomUUID();
-        return record;
-    }
-
-    function openModal(modal) {
-        modal.show();
-    }
-
-    function closeModal(modal) {
-        modal.hide();
-    }
-
     function init(container) {
         const component = createComponent();
         container.appendChild(component);
@@ -66,12 +17,58 @@ export default function (container, data, callback) {
         const modal = new Modal(document.getElementById("add-modal"));
 
         const form = document.getElementById('my-modal');
-        form.onsubmit = (event) => handleSubmit(event, form, modal);
+        form.onsubmit = (event) => handleSubmit(event, form, modal, data, callback);
 
         return {component, open: () => openModal(modal)};
     }
 
-
-
     return init(container);
 }
+
+const closeModal = (modal) => modal.hide();
+
+const openModal = (modal) => modal.show();
+
+const validateField = (value, regex, errorElementId) => {
+    const errorElement = document.getElementById(errorElementId);
+    if (!regex.test(value)) {
+        errorElement.classList.remove("hidden");
+        return false;
+    } else {
+        errorElement.classList.add("hidden");
+        return true;
+    }
+}
+
+const handleSubmit = (event, form, modal, data, callback) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    if (!validateField(formData.get("inn"), /^\d{11}$/, 'innError')) {
+        return;
+    }
+
+    if (!validateField(formData.get("kpp"), /^\d{9}$/, 'kppError')) {
+        return;
+    }
+
+    const record = createRecord(formData);
+    data.push(record);
+
+    callback();
+    form.reset();
+    closeModal(modal);
+}
+
+const createRecord = (formData) => {
+    const record = {};
+    formData.forEach((value, key) => {
+        record[key] = value;
+    });
+    record.id = crypto.randomUUID();
+    return record;
+}
+
+
+
